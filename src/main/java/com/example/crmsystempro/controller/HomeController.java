8d19f92cba9694c3bca7bcb7d4aabe2ff07b39d8
Zharaskhan <m_jasik_2005@mail.ru>
2025-10-19 22:18:04 +0600

    controller, pages updated

--- src/main/java/com/example/crmsystempro/controller/HomeController.java
+++ src/main/java/com/example/crmsystempro/controller/HomeController.java
@@ -2,18 +2,29 @@ package com.example.crmsystempro.controller;
 
 import com.example.crmsystempro.entities.ApplicationRequest;
 import com.example.crmsystempro.services.ApplicationRequestService;
+import com.example.crmsystempro.services.CourseService;
+import com.example.crmsystempro.services.OperatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @Autowired
     private ApplicationRequestService requestService;
+
+    @Autowired
+    private CourseService coursesService;
+
+    @Autowired
+    private OperatorService operatorService;
 
     @GetMapping("/")
     public String home(Model model) {
@@ -34,7 +45,8 @@ public class HomeController {
     }
 
     @GetMapping("/add-request")
-    public String addRequestPage() {
+    public String addRequestPage(Model model) {
+        model.addAttribute("courses", coursesService.getAllCourses());
         return "add-request";
     }
 
@@ -47,8 +59,26 @@ public class HomeController {
     @GetMapping("/details/{id}")
     public String details(@PathVariable(name = "id") Long id, Model model) {
         model.addAttribute("request", requestService.getRequestById(id));
+        model.addAttribute("operators", operatorService.getAllOperators());
         return "details";
     }
+
+
+    @PostMapping("/assign-operators")
+    public String assignOperators(@RequestParam(name = "requestId") Long requestId,
+                                  @RequestParam(name = "operatorId") List<Long> operatorIds) {
+        requestService.assignOperators(requestId, operatorIds);
+        return "redirect:/details/" + requestId;
+    }
+
+
+    @PostMapping("/remove-operator")
+    public String removeOperator(@RequestParam(name = "requestId") Long requestId,
+                                 @RequestParam(name = "operatorId") Long operatorId) {
+        requestService.removeOperatorFromRequest(requestId, operatorId);
+        return "redirect:/details/" + requestId;
+    }
+
 
     @PostMapping("/process-request/{id}")
     public String processRequest(@PathVariable(name = "id") Long id) {
